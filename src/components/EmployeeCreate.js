26d91c9963f3be68433801d388eafafa3a1f77be
joebelmonte <joseph.belmonte@gmail.com>
2017-09-27 15:48:57 -0400

    Name and number now working on employee create form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
+    console.log('in EmployeeCreate and this.props is ', this.props);
     return (
       <Card>
         <CardSection>
-          <Input label="Name" placeholder="Jane" />
+          <Input
+            label="Name"
+            placeholder="Jane"
+            value={this.props.name}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'name', value: text })}
+          />
         </CardSection>
 
         <CardSection>
-          <Input label="Phone" placeholder="555-555-5555" />
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={text => this.props.employeeUpdate({ prop: 'phone', value: text })}
+          />
         </CardSection>
 
         <CardSection />
@@ -23,4 +36,10 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = state => {
+  console.log('in mapStateToProps');
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
