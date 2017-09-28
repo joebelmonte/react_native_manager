2b0f2489a00a1e5db95a73b9cdf0a8e0c9a8448e
joebelmonte <joseph.belmonte@gmail.com>
2017-09-28 17:12:36 -0400

    Turned employee list from object to array in Employee List

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { ListView, View, Text } from 'react-native';
+import _ from 'lodash';
+import { employeesFetch } from '../actions';
 
 class EmployeeList extends Component {
+  componentWillMount() {
+    this.props.employeesFetch();
+    this.createDataSource(this.props);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps);
+  }
+
+  createDataSource({ employees }) {
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 !== r2
+    });
+
+    this.dataSource = ds.cloneWithRows(employees);
+  }
+
   render() {
+    console.log('this.props is ', this.props);
     return (
       <View>
         <Text>Employee list</Text>
@@ -17,4 +38,10 @@ class EmployeeList extends Component {
   }
 }
 
-export default EmployeeList;
+const mapStateToProps = state => {
+  const employees = _.map(state.employees, (val, uid) => ({ ...val, uid }));
+
+  return { employees };
+};
+
+export default connect(mapStateToProps, { employeesFetch })(EmployeeList);
